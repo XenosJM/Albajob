1ae0edcfcadcdec63e922c56852c71feba86945a
dmasisissi <gusdn310@naver.com>
2024-08-19 13:08:03 +0900

    컨트롤러 작성중

--- src/main/java/com/web/albajob/controller/CompanyMemberController.java
+++ src/main/java/com/web/albajob/controller/CompanyMemberController.java
@@ -2,12 +2,83 @@ package com.web.albajob.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.web.albajob.domain.CompanyMemberVO;
 import com.web.albajob.service.CompanyMemberService;
 
+import lombok.extern.log4j.Log4j;
+
 @Controller
+@RequestMapping(value = "/member")
+@Log4j
 public class CompanyMemberController {
+	
 	@Autowired
 	private CompanyMemberService companyMemberService;
+	
+	@GetMapping("/regist")
+	public void companyRegistGet() {
+		log.info("joinGet()");
+	}	
+	@PostMapping("/regist")
+	public String companyRegistPOST(CompanyMemberVO vo) {
+		log.info(vo);
+		int result = companyMemberService.insertMember(vo);
+		log.info(result+"행 삽입");
+		return "redirect:/auth/login";
+	}
+	
+	@GetMapping("/findByPhone")
+	public void findByPhoneGet() {
+		log.info("findByPhoneGet()");
+	}
+	
+	@PostMapping("/findByPhone")
+	public String findByPhone(int userphone) {
+		return companyMemberService.findUserNameByPhone(userphone);
+	}
+
+	@GetMapping("/findByMail")
+	public void findByMailGet() {
+		log.info("findByMailGet()");
+	}
+	
+	@PostMapping("/findByMail")
+	public String findByMail(String userMail) {
+		return companyMemberService.findUserNameByMail(userMail);
+	}
+	
+	@GetMapping("/findByPW")
+	public void findByPWGet() {
+		log.info("findByPWGet()");
+	}
+	
+	@PostMapping("/findByPW")
+	public String findByPW(String userName) {
+		return companyMemberService.findUserPW(userName);
+	}
+	
+	@GetMapping("/companyUpdate")
+	public void companyUpdateGET() {
+		log.info("companyUpdateGET()");
+	}
+	
+	@PostMapping("/companyUpdate")
+	public String companyUpdate(CompanyMemberVO vo) {
+		int result = companyMemberService.updateMember(vo);
+		log.info(result+"행 수정 완료");
+		return "redirect:/member/info";
+	}
+	
+	@PostMapping("/deleteCompany")
+	public String deleteCompany(CompanyMemberVO vo) {
+		int result = companyMemberService.deleteMember(vo.getUserId());
+		log.info(result+"행 삭제 완료");
+		return "redirect:/main";
+	}
+	
 
 }
