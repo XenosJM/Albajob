bdc5739d1927da0e311a3ca9d5daa766e43f3b20
Jeyoung Woo <dnwodud3@gmail.com>
2024-08-19 12:41:29 +0900

    이력서 매퍼 까지 작성 완료

---
+++ src/main/java/com/web/albajob/persistence/ResumeMapper.java
@@ -0,0 +1,27 @@
+package com.web.albajob.persistence;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.web.albajob.domain.ResumeVO;
+
+@Mapper
+public interface ResumeMapper {
+	
+
+	int insertResume(ResumeVO resumeVO);
+		 
+
+	List<ResumeVO> selectByResume(int productId);
+	
+
+	List<ResumeVO> selectByResumeUserId(int productId);
+	
+
+	int updateInquiry(ResumeVO resumeVO);
+	 
+
+	int deleteResume(@Param("userName")String userName, @Param("resumeId")int resumeId);
+}
---
+++ src/main/webapp/WEB-INF/views/resume/list.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>아력서 리스트/title>
+</head>
+<body>
+
+	<h1>이력서 관리</h1>
+	<p>이력서는 최대 6개까지 등록할 수 있습니다.</p>
+	<p>지원 후 수정한 이력서 내용(회원정보 제외)은 지원한 이력서에는 반영 되지 않습니다.</p>
+	
+	
+
+</body>
+</html>
